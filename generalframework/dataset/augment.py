e3a50ccf440bd53fb7d64699c32d50d485b6a77f
Guillermo <guilled52@gmail.com>
2019-01-26 12:26:44 -0500

    add config file for vat experiment on cityscapes

--- generalframework/dataset/augment.py
+++ generalframework/dataset/augment.py
@@ -195,6 +195,16 @@ class RandomSized(object):
         return self.crop(*self.scale(img, mask))
 
 
+class RandomHorizontallyFlip(object):
+    def __init__(self, p):
+        self.p = p
+
+    def __call__(self, img, mask):
+        if random.random() < self.p:
+            return (img.transpose(Image.FLIP_LEFT_RIGHT), mask.transpose(Image.FLIP_LEFT_RIGHT))
+        return img, mask
+
+
 key2aug = {
     "rcrop": RandomCrop,
     "scale": Scale,
@@ -202,6 +212,7 @@ key2aug = {
     "rsizecrop": RandomSizedCrop,
     "rotate": RandomRotate,
     "ccrop": CenterCrop,
+    "hflip": RandomHorizontallyFlip,
 }
 
 
--- train_vat.py
+++ train_vat.py
@@ -1,6 +1,6 @@
 import warnings
 from pprint import pprint
-from generalframework.dataset import get_dataloaders, extract_patients
+from generalframework.dataset import get_dataloaders, extract_patients, get_cityscapes_dataloaders
 from generalframework.loss import get_loss_fn
 from generalframework.models import Segmentator
 from generalframework.trainer import VatTrainer
@@ -20,20 +20,23 @@ warnings.filterwarnings('ignore')
 parser_args = yaml_parser()
 print('->>Input args:')
 pprint(parser_args)
-with open('config_vat.yaml', 'r') as f:
+# config_file = 'config_vat.yaml'
+config_file = 'configs/config_vattrain_nat_img.yaml'
+with open(config_file, 'r') as f:
     config = yaml.load(f.read())
 print('->> Merged Config:')
 config = dict_merge(config, parser_args, True)
 pprint(config)
 
-dataloders = get_dataloaders(config['Dataset'], config['Lab_Dataloader'])
+dataloaders = get_cityscapes_dataloaders(config['Dataset'], config['Lab_Dataloader'])
+# dataloders = get_dataloaders(config['Dataset'], config['Lab_Dataloader'])
 
 # lab_dataloader = extract_patients(dataloders['train'], [str(x) for x in range(1, 50)])
 # unlab_dataloader = get_dataloaders(config['Dataset'], config['Unlab_Dataloader'], quite=True)['train']
 # unlab_dataloader = extract_patients(unlab_dataloader, [str(x) for x in range(50, 100)])
-dataloders = {'lab': dataloders['train'],
-              'unlab': dcopy(dataloders['train']),
-              'val': dataloders['val']}
+# dataloaders = {'lab': dataloders['train'],
+#               'unlab': dcopy(dataloders['train']),
+#               'val': dataloders['val']}
 
 model = Segmentator(arch_dict=config['Arch'], optim_dict=config['Optim'], scheduler_dict=config['Scheduler'])
 
@@ -42,7 +45,7 @@ with warnings.catch_warnings():
     criterion = get_loss_fn(config['Loss'].get('name'), **{k: v for k, v in config['Loss'].items() if k != 'name'})
 
 vattrainer = VatTrainer(segmentator=model,
-                        dataloaders=dataloders,
+                        dataloaders=dataloaders,
                         criterion=criterion,
                         **config['Trainer'],
                         whole_config=config)
