45bb9370ba4014bbfb05f39e528adeffa0e05d00
Jizong Ubuntu <jizong.peng.1@etsmtl.net>
2019-01-27 16:23:17 -0500

    temporal check

--- train_cotraining.py
+++ train_cotraining.py
@@ -27,13 +27,10 @@ config = dict_merge(config, parser_args, True)
 pprint(config)
 
 dataloders = get_dataloaders(config['Dataset'], config['Lab_Dataloader'])
-lab_dataloader1 = dcopy(dataloders['train'])
-lab_dataloader2 = dcopy(dataloders['train'])
-unlab_dataloader = dcopy(dataloders['train'])
-# lab_dataloader1 = extract_patients(dataloders['train'], [str(x) for x in range(1, 26)])
-# lab_dataloader2 = extract_patients(dataloders['train'], [str(x) for x in range(26, 50)])
-# unlab_dataloader = get_dataloaders(config['Dataset'], config['Unlab_Dataloader'], quite=True)['train']
-# unlab_dataloader = extract_patients(unlab_dataloader, [str(x) for x in range(50, 100)])
+lab_dataloader1 = extract_patients(dataloders['train'], [str(x) for x in range(1, 26)])
+lab_dataloader2 = extract_patients(dataloders['train'], [str(x) for x in range(26, 50)])
+unlab_dataloader = get_dataloaders(config['Dataset'], config['Unlab_Dataloader'], quite=True)['train']
+unlab_dataloader = extract_patients(unlab_dataloader, [str(x) for x in range(50, 100)])
 
 val_dataloader = dataloders['val']
 
