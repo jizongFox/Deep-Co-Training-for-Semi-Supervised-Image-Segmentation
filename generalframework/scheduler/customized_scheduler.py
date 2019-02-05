f6b4704383d3965712c198a72b8133be53a5f6b0
Guillermo <guilled52@gmail.com>
2019-02-05 15:42:31 -0500

    add RampDownScheduler

--- generalframework/scheduler/customized_scheduler.py
+++ generalframework/scheduler/customized_scheduler.py
@@ -42,6 +42,34 @@ class RampScheduler(Scheduler):
         return max_val * np.exp(mult * (1. - float(epoch) / max_epochs) ** 2)
 
 
+class RampDownScheduler(RampScheduler):
+
+    def __init__(self, max_epoch, max_value, ramp_mult, min_val, cutoff, last_epoch=-1):
+        super().__init__(last_epoch)
+        self.max_epoch = max_epoch
+        self.max_value = max_value
+        self.mult = ramp_mult
+        self.epoch = 0
+        self.min_val = min_val
+        self.cutoff = cutoff
+
+    def step(self):
+        self.epoch += 1
+
+    @property
+    def value(self):
+        return self.ramp_down(self.epoch, self.max_epoch, self.max_value, self.mult, self.min_val, self.cutoff)
+
+    @staticmethod
+    def ramp_down(epoch, max_epochs, max_val, mult, min_val, cutoff):
+        assert cutoff < max_epochs
+        if epoch == 0:
+            return 1.
+        elif epoch >= cutoff:
+            return min_val
+        return 1 - max_val * np.exp(mult * (1. - float(epoch) / max_epochs) ** 2)
+
+
 class ConstantScheduler(Scheduler):
 
     def __init__(self, max_value=1.0, last_epoch=-1):
