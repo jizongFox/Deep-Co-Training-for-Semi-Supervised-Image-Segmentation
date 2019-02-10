a374ff04118e4915e663a2694d8cdf1fdee468c8
Jizong Ubuntu <jizong.peng.1@etsmtl.net>
2019-02-10 17:07:57 -0500

    tested the pipeline

--- script/ACDC_sanity_check.sh
+++ script/ACDC_sanity_check.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -e
 cd ..
+time=$(date +'%m%d_%H:%M')
+gitcommit_number=$(git rev-parse HEAD)
+gitcommit_number=${gitcommit_number:0:8}
 
-max_peoch=2
-
+max_peoch=100
 data_aug=None
 net=unet
 logdir=cardiac/$net"_cotraining2models_test"
-mkdir -p archives/$logdir
-## Fulldataset baseline
+
 
 FS(){
 gpu=$1
@@ -29,7 +30,6 @@ currentfoldername=PS
 rm -rf runs/$logdir/$currentfoldername
 
 CUDA_VISIBLE_DEVICES=$gpu python train_ACDC_cotraining.py Trainer.save_dir=runs/$logdir/$currentfoldername \
-
 Trainer.max_epoch=$max_peoch Dataset.augment=$data_aug \
 StartTraining.train_adv=False StartTraining.train_jsd=False \
 Lab_Partitions.label="[[1,41],[21,61]]" Lab_Partitions.unlabel="[61,101]" \
@@ -92,10 +92,17 @@ rm -rf archives/$logdir/$currentfoldername
 mv -f runs/$logdir/$currentfoldername archives/$logdir
 }
 
-FS 0
-#&
-Partial 0
+Summary(){
+subfolder=$1
+gpu=$2
+echo CUDA_VISIBLE_DEVICES=$gpu python Summary.py --input_dir archives/$logdir/$subfolder
+CUDA_VISIBLE_DEVICES=$gpu python Summary.py --input_dir archives/$logdir/$subfolder
+}
 
+mkdir -p archives/$logdir
+
+FS 0
+Partial 0
 Partial_alldata 0
 JSD 0
 ADV 0
@@ -111,14 +118,6 @@ python generalframework/postprocessing/plot.py --folders archives/$logdir/FS/ \
 archives/$logdir/PS_alldata/ archives/$logdir/PS/ \
 archives/$logdir/JSD/  archives/$logdir/ADV/ archives/$logdir/JSD_ADV/  --file val_dice.npy --axis 1 2 3 --postfix=model1 --seg_id=1 --y_lim 0.3 0.9
 
-## ensemble
-Summary(){
-subfolder=$1
-gpu=$2
-echo CUDA_VISIBLE_DEVICES=$gpu python Summary.py --input_dir archives/$logdir/$subfolder
-CUDA_VISIBLE_DEVICES=$gpu python Summary.py --input_dir archives/$logdir/$subfolder
-}
-#
 Summary FS 0
 Summary PS 0
 Summary PS_alldata 0
@@ -128,4 +127,4 @@ Summary JSD_ADV 0
 
 python generalframework/postprocessing/report.py --folder=archives/$logdir/ --file=summary.csv
 
-#zip -rq archives/$logdir".zip" archives/$logdir
+zip -rq archives/$logdir"_"$time"_"$gitcommit_number".zip" archives/$logdir
