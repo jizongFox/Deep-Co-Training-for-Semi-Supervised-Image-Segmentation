87cf2093951a2e39e32b09680253d8ac932c4077
jizong <pengjizong@sjtu.edu.cn>
2019-02-17 13:56:29 -0500

    add run script for sbatch

--- script/run_search.sh
+++ script/run_search.sh
@@ -2,11 +2,12 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --job-name="acdc_search"
-#SBATCH --cpus-per-task=24
+#SBATCH --cpus-per-task=12
 #SBATCH --mem=32000M
-#SBATCH --time=0-35:30
+#SBATCH --time=0-36:00
 #SBATCH --account=def-chdesa
 #SBATCH --mail-user=jizong.peng.1@etsmtl.net
 #SBATCH --mail-type=ALL
 jobname=$1
-sbatch  --job-name=$jobname ACDC_search_params.sh $jobname
+echo $jobname
+sbatch  --job-name=$jobname  --nodes=1  --gres=gpu:1  --cpus-per-task=12   --mem=32000M  --time=0-36:00 --account=def-chdesa --mail-user=jizong.peng.1@etsmtl.net --mail-type=ALL   ACDC_search_params.sh $jobname
