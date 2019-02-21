23b82f138cd89eccea62bc5accf7bebe67cc06a9
Jizong Ubuntu <jizong.peng.1@etsmtl.net>
2019-02-21 11:39:26 -0500

    debug on lab partitons

--- train_ACDC_cotraining.py
+++ train_ACDC_cotraining.py
@@ -46,44 +46,50 @@ def get_models(config):
 
 def get_dataloders(config):
     dataloders = get_ACDC_dataloaders(config['Dataset'], config['Lab_Dataloader'])
+    try:
+        partition_ratio = config['Lab_Partitions']['partition_sets']
+        lab_ids, unlab_ids = create_partitions(partition_ratio)
+        partition_overlap = config['Lab_Partitions']['partition_overlap']
+        rd_idx = np.random.permutation(range(*lab_ids))
+        overlap_idx = np.random.choice(rd_idx, size=int(partition_overlap * range(*lab_ids).__len__()),
+                                       replace=False)
+        exclusive_idx = [x for x in rd_idx if x not in overlap_idx]
+        n_splits = config['Lab_Partitions']['label'].__len__()
+        exclusive_samples = int(exclusive_idx.__len__() / n_splits)
+        excl_indx = [exclusive_idx[i * exclusive_samples: (i + 1) * exclusive_samples] for i in range(n_splits)]
 
-    partition_ratio = config['Lab_Partitions']['partition_sets']
-    lab_ids, unlab_ids = create_partitions(partition_ratio)
-    # print(list(lab_ids))
-    # print(list(unlab_ids))
+        lab_partitions = [np.hstack((overlap_idx, np.array(excl_indx[idx]))) for idx in range(n_splits)]
+        labeled_dataloaders = []
+        for idx_lst in lab_partitions:
+            labeled_dataloaders.append(extract_patients(dataloders['train'], [str(int(x)) for x in idx_lst]))
 
-    partition_divers = config['Lab_Partitions']['partition_diversity']
-    rd_idx = np.random.permutation(range(*lab_ids))
-    overlap_idx = np.random.choice(rd_idx, size=int(partition_divers / 100 * range(*lab_ids).__len__()),
-                                   replace=False)
-    exclusive_idx = [x for x in rd_idx if x not in overlap_idx]
+        unlab_dataloader = get_ACDC_dataloaders(config['Dataset'], config['Unlab_Dataloader'], quite=True)['train']
+        unlab_dataloader = extract_patients(unlab_dataloader, [str(x) for x in range(*unlab_ids)])
+        val_dataloader = dataloders['val']
+        print('labeled_image_number:',len(range(*lab_ids)), 'unlab_image_number:',len(range(*unlab_ids)))
+        from functools import reduce
+        print(f'{len(lab_partitions)} datasets with overlap labeled image number', len(reduce(lambda x,y: x&y,list(map(lambda x:set(x), lab_partitions)))))
+        return labeled_dataloaders, unlab_dataloader, val_dataloader
+    except:
+        labeled_dataloaders = []
+        for i in config['Lab_Partitions']['label']:
+            labeled_dataloaders.append(extract_patients(dataloders['train'], [str(x) for x in range(*i)]))
 
-    n_splits = 2
-    overlap_samples = int(overlap_idx.size / n_splits)
-    over_indx = [overlap_idx[i * overlap_samples: (i+1) * overlap_samples] for i in range(n_splits)]
-
-    exclusive_samples = int(exclusive_idx.__len__() / n_splits)
-    excl_indx = [exclusive_idx[i * exclusive_samples: (i + 1) * exclusive_samples] for i in range(n_splits)]
-
-    lab_partitions = [np.hstack((over_indx[idx], np.array(excl_indx[idx]))) for idx in range(n_splits)]
-    labeled_dataloaders = []
-    for idx_lst in lab_partitions:
-        labeled_dataloaders.append(extract_patients(dataloders['train'], [str(x) for x in idx_lst]))
-
-    unlab_dataloader = get_ACDC_dataloaders(config['Dataset'], config['Unlab_Dataloader'], quite=True)['train']
-    unlab_dataloader = extract_patients(unlab_dataloader, [str(x) for x in range(*unlab_ids)])
-    val_dataloader = dataloders['val']
-    return labeled_dataloaders, unlab_dataloader, val_dataloader
+        unlab_dataloader = get_ACDC_dataloaders(config['Dataset'], config['Unlab_Dataloader'], quite=True)['train']
+        unlab_dataloader = extract_patients(unlab_dataloader,
+                                            [str(x) for x in range(*config['Lab_Partitions']['unlabel'])])
+        val_dataloader = dataloders['val']
+        return labeled_dataloaders, unlab_dataloader, val_dataloader
 
 
-def create_partitions(partition_ratio=60):
-    lab_ids = [1, partition_ratio+1]
-    unlab_ids = [partition_ratio+1, 101]
-    # print('Labeled and unlabeled partition is: {}'.format(partition_ratio))
+def create_partitions(partition_ratio=0.6):
+    lab_ids = [1,int(100* partition_ratio+1)]
+    unlab_ids = [int(100*partition_ratio+1), 101]
     return lab_ids, unlab_ids
 
 
 labeled_dataloaders, unlab_dataloader, val_dataloader = get_dataloders(config)
+
 
 segmentators = get_models(config)
 
