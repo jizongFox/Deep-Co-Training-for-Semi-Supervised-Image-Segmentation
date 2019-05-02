992adc29910d3b8c9ced339cb22411662027f4f0
JizongXPS <jizong.peng.1@etsmtl.net>
2019-05-02 16:33:29 -0400

    try to improve

--- generalframework/dataset/test_tensorAugment.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-
-
-class TestTensorAugment(TestCase):
-    def test_TensorAugment(self):
-        self.fail()
--- test/test_kappa.py
+++ test/test_kappa.py
@@ -1,14 +1,14 @@
 import torch
+
 from generalframework.metrics import KappaMetrics
 
+kappa_Meter = KappaMetrics()
 
-kappa_Meter= KappaMetrics()
-
-for i in range (100):
+for i in range(100):
     predict1 = torch.randn(2, 4, 256, 256)
     predict2 = torch.randn(2, 4, 256, 256)
     # gt = torch.randint(0, 4, size=(2, 256, 256))
     gt = predict1.max(1)[1]
-    kappa_Meter.add(predicts=[predict1.max(1)[1],predict2.max(1)[1]],target=gt,considered_classes=[1,2,3])
+    kappa_Meter.add(predicts=[predict1.max(1)[1], predict2.max(1)[1]], target=gt, considered_classes=[1, 2, 3])
 
 print(kappa_Meter.value())
